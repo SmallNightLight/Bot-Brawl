dde7e76ef978e8a560c65ef18eed8c1a5bfb374c
SmallNightLight <matthiaskruis@gmail.com>
2023-11-23 18:58:31 +0100

    Fix

--- Assets/Scripts/Scriptables/Data/DataPoints/BotPartData.cs
+++ Assets/Scripts/Scriptables/Data/DataPoints/BotPartData.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 namespace ScriptableArchitecture.Data
 {
     [System.Serializable]
-    [CreateAssetMenu(fileName = "Default", menuName = "Scriptables/Variables/Parts/Default")]
-    public class BotPartDataD : PartData
-    {
-    }
-
-    [System.Serializable]
     public class BotPartData<T> : PartData where T : IPartSettings
     {
         public T PartSettings;

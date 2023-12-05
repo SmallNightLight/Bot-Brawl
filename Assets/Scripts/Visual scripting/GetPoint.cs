354cbc19c0208af845dad359add9b1e460d5d3a7
SmallNightLight <matthiaskruis@gmail.com>
2023-12-05 03:08:43 +0100

    Number input fields

--- Assets/Scripts/Visual scripting/GetPoint.cs
+++ Assets/Scripts/Visual scripting/GetPoint.cs
@@ -2,14 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using System;
 
 public class GetPoint : MonoBehaviour
 {
     [HideInInspector] public Color BaseColor;
 
-    [HideInInspector] public BaseGet DefaultGet;
-    [HideInInspector] public DisplayGet ChildGet;
-    
+    public BaseGet DefaultGet;
+
+    private DisplayGet _childGet;
+
+    [HideInInspector] public DisplayGet ChildGet
+    {
+        get
+        {
+            return _childGet;
+        }
+        set
+        {
+            _childGet = value;
+            ReloadInputField();
+        }
+    }
+
+    bool IsDerivedFrom(object obj, Type baseType)
+    {
+        return obj is BaseGet && obj.GetType().IsSubclassOf(baseType);
+    }
+
+    public bool IsNumber = false;
+    [SerializeField] private TMP_InputField _numberInputField;
+
     public DisplayDo MainDo
     {
         get
@@ -25,6 +49,32 @@ public class GetPoint : MonoBehaviour
     {
         ALLGETPOINTS.Add(GetComponent<RectTransform>());
         BaseColor = GetComponent<Image>().color;
+
+        if (DefaultGet == null)
+        {
+            IsNumber = false;
+            return;
+        }
+
+        IsNumber = IsDerivedFrom(DefaultGet, typeof(BaseGetNumber));
+        ReloadInputField();
+    }
+
+    private void ReloadInputField()
+    {
+        bool current = false;
+
+        DisplayGet mainGet = GetComponentInParent<DisplayGet>();
+        DisplayDo mainDo = MainDo;
+
+        if (mainGet != null)
+            current = !mainGet.IsDefaultNode;
+        else if (mainDo != null)
+            current = !mainDo.IsDefaultNode;
+
+        current &= ChildGet == null && IsNumber;
+
+        _numberInputField.gameObject.SetActive(current);
     }
 
     private void OnDestroy()
@@ -35,5 +85,10 @@ public class GetPoint : MonoBehaviour
     public bool HasChild()
     {
         return ChildGet != null;
+    }
+
+    public float GetNumberValue()
+    {
+        return float.Parse(_numberInputField.text.Replace(',', '.'));
     }
 }
